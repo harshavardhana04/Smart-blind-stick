a5bd35f6f998ead33c644a1cdcfa62041082046d
Harshavardhana m l <harsha23176@gmail.com>
2022-06-28 14:44:40 +0530

    Update ard_comm.ino

--- ard_comm.ino
+++ ard_comm.ino
@@ -1,16 +1,18 @@
 #define triPin 2
 #define Echo 3
-#define Buzzer 11
+#define vibrator 9
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(Echo,INPUT);
   pinMode(triPin,OUTPUT);
+  pinMode(vibrator,OUTPUT);
   Serial.begin(9600);
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  float duration,distance;
+  float duration;
+  int distance;
   digitalWrite(triPin,HIGH);
   delayMicroseconds(10);
   digitalWrite(triPin,LOW);
@@ -18,11 +20,13 @@ void loop() {
   distance=duration*0.034/2;
 
   //Serial.print("distance = ");
-  Serial.println(distance);
+  Serial.println(int(distance));
    
-  if(distance<50 and distance>25){
-    tone(Buzzer,200,300);
-  }
+  if(distance<20){
+    digitalWrite(vibrator,HIGH);
+    delay(100);
+    digitalWrite(Vibrator,LOW);
+  }/*
   if(distance<25 and distance>10){
     tone(Buzzer,500,300);
   }
@@ -31,5 +35,5 @@ void loop() {
   }
   else{
     tone(Buzzer,2000,300);
-  }
+  }*/
 }
